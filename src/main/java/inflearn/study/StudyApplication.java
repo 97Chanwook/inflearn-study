e5a69066a20d8dfae749e4c53f756f44d1c6557c
97Chanuk <chanuk9531@daum.net>
2024-01-02 21:41:34 +0900

    Hello Servlet 추가
    Hello Servlet 추가 완료

--- src/main/java/inflearn/study/StudyApplication.java
+++ src/main/java/inflearn/study/StudyApplication.java
@@ -2,7 +2,9 @@ package inflearn.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	// 서블릿 자동 등록 (스프링 부트에서 서블릿을 직접 등록해서 사용할 수 있도록 함. 잠시 사용)
 @SpringBootApplication
 public class StudyApplication {
 
